a7c22427e47ffe725c9a8832ade23922117b45b9
Francesco Peluso <59664699+francescopeluso@users.noreply.github.com>
2022-04-30 11:18:04 +0200

    new commit

--- public/js/slide-page-counter.js
+++ public/js/slide-page-counter.js
@@ -1,31 +1,73 @@
 const up_button = document.querySelector('#up-arrow');
 const down_button = document.querySelector('#down-arrow');
+const counter_sect = document.querySelector('#counter');
 
 let counter = null;
 let slides = null;
+
+document.onkeydown = (event) => {
+    switch (event.keyCode) {
+        case 38:    // up key pressed
+            if (counter !== 0) {
+                counter--;
+                slides[counter].scrollIntoView();
+                counter_sect.innerHTML = (counter + 1) + "/" + slides.length;
+            }
+        
+            buttonOpacity();
+            break;
+
+        case 40:    // down key pressed
+            if (counter !== slides.length) {
+                counter++;
+                slides[counter].scrollIntoView();
+                counter_sect.innerHTML = (counter + 1) + "/" + slides.length;
+            }
+        
+            buttonOpacity();
+            break;
+    }
+}
+
+function buttonOpacity() {
+    if (counter == 0) {
+        up_button.children[0].style.opacity = "0.5";
+    } else {
+        up_button.children[0].style.opacity = "1";
+    }
+    if (counter == slides.length - 1) {
+        down_button.children[0].style.opacity = "0.5";
+    } else {
+        down_button.children[0].style.opacity = "1";
+    }
+}
 
 window.onload = () => {
     counter = 0;
     slides = document.getElementsByTagName("section");
     slides[counter].scrollIntoView();
+    counter_sect.innerHTML = (counter + 1) + "/" + slides.length;
+    buttonOpacity();
 }
 
 up_button.addEventListener("click", () => {
-    if (counter === 0) {
-        slides[counter].scrollIntoView();
-    } else {
+    if (counter !== 0) {
         counter--;
         slides[counter].scrollIntoView();
+        counter_sect.innerHTML = (counter + 1) + "/" + slides.length;
     }
 
-})
+    buttonOpacity();
+
+});
 
 down_button.addEventListener("click", () => {
-    if (counter === slides.length) {
-        slides[counter].scrollIntoView();
-    } else {
+    if (counter !== slides.length) {
         counter++;
         slides[counter].scrollIntoView();
+        counter_sect.innerHTML = (counter + 1) + "/" + slides.length;
     }
 
-})
+    buttonOpacity();
+
+});
