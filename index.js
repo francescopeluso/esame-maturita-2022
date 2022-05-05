1560333882dc7fba68665d4129fac43f1f465382
Francesco Peluso <59664699+thatsfrankie@users.noreply.github.com>
2022-05-05 12:12:23 +0200

    update

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/favicon.ico', express.static('favicon.ico'));
 
 // handling GET requests
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.render(__dirname + '/views/index.ejs');
 });
 
+app.get('/italiano', (req, res, file = JSON.parse(__dirname + '/content/italiano.json')) => {
+    res.render(__dirname + '/views/essay.ejs', {file: this.file});
+});
+
 // HTTP 404 Error (Not found)
-app.use(function(req, res, next) {
-    res.status(404).sendFile(__dirname + '/404.html');
+app.use((req, res, next) => {
+    res.status(404).send("unavailable");
 });
 
 // Start ExpressJS web server on http://localhost:8080/
